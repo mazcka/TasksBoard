9aba9184bcb18be8f3c0c3ee257b122af9448ccb
mazcka <mazcka@gmail.com>
2022-11-03 13:42:49 +0200

    UI improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { TaskItemComponent } from './board/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
     NavigationBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
